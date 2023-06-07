1e9bf05445201efde2f4fb3188cc16cbac167f91
AnnRuab <99259534+AnnRuab@users.noreply.github.com>
2023-06-07 22:05:18 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-class Configuration(object):
-    DEBUG = True
-    SECRET_KEY = 'ImNhODJhMTg4YzVkNGJjNDMxZThiMDE0ZDIwZDkzMjk5MDY4YTRiZTEi.ZE6mJQ.R1KoHw0hNwg4TvTO0R_KHSeFqnc'
-    CSRF_ENABLED = True
